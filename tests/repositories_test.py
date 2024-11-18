43b0a4c4bdfb8d665c5dbcfc1323088df3745cc3
Eduardo Pizorno <pizorno@gmail.com>
2024-11-18 17:34:08 -0300

    :white_check_mark: chore: Testando repositório pets

--- tests/repositories_test.py
+++ tests/repositories_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from find_a_friend.models.sqlite.repositories.pets_repository import (
     PetsRepository,
 )
@@ -8,7 +10,15 @@ from find_a_friend.models.sqlite.settings.connection import (
 db_connection_handler.connect_to_db()
 
 
+@pytest.mark.skip(reason='Interação com o banco')
 def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
     print(response)
+
+
+@pytest.mark.skip(reason='Interação com o banco')
+def test_delete_pet():
+    name = 'belinha'
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
