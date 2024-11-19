b48958c71a0fb342873c72b67707302c58463d01
Eduardo Pizorno <pizorno@gmail.com>
2024-11-19 09:16:23 -0300

    :white_check_mark: chore: Testando repositório pets - delete_pet

--- tests/pets_repository_test.py
+++ tests/pets_repository_test.py
@@ -37,3 +37,14 @@ def test_list_pets():
     mock_connection.session.all.assert_called_once()
     mock_connection.session.filter.assert_not_called()
     assert response[0].name == 'dog'
+
+
+def test_delete_pet():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+    repo.delete_pets('petName')
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(
+        PetsTable.name == 'petName'
+    )
+    mock_connection.session.delete.assert_called_once()
