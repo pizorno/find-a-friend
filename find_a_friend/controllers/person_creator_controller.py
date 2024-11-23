11b4c507a226c12c030d97c5a5143776e351719a
Eduardo Pizorno <pizorno@gmail.com>
2024-11-23 09:33:42 -0300

    :sparkles: feat: Implementando person_creator_controller com seus testes

--- find_a_friend/controllers/person_creator_controller.py
+++ find_a_friend/controllers/person_creator_controller.py
@@ -1,3 +1,5 @@
+import re
+
 from find_a_friend.models.sqlite.interfaces.people_repository import (
     PeopleRepositoryInterface,
 )
@@ -6,3 +8,30 @@ from find_a_friend.models.sqlite.interfaces.people_repository import (
 class PersonCreatorController:
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
+
+    def create(self, person_info: dict) -> dict:
+        first_name = person_info['first_name']
+        last_name = person_info['last_name']
+        age = person_info['age']
+        pet_id = person_info['pet_id']
+        self.__validate_names(first_name, last_name)
+        self.__insert_person(first_name, last_name, age, pet_id)
+        formated_response = self.__format_response(person_info)
+        return formated_response
+
+    def __validate_names(self, first_name: str, last_name: str) -> None:
+        non_valid_caracteres = re.compile(r'[^a-zA-Z]')
+        if non_valid_caracteres.search(first_name) or non_valid_caracteres.search(last_name):
+            raise Exception('Nome da pessoa inválido')
+
+    def __insert_person(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+        self.__people_repository.insert_person(first_name, last_name, age, pet_id)
+
+    def __format_response(self, person_info: dict) -> dict:
+        return {
+            'data': {
+                'type': 'Person',
+                'count': 1,
+                'attributes': person_info
+            }
+        }
